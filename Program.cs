b98b2ca400d53a50b5377fac6b5b132920616c68
[sidney.batista] <[sidneycezar96@gmail.com]>
2022-09-25 13:15:33 -0300

    fix:
    Ajuste para apresentação das informações conforme proposto.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Celular Nokia");
+Nokia nokia = new Nokia(numero: "220532331", modelo: "NOKIA 5.3", imei: "NK007", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Celular Iphone");
+Iphone iphone = new Iphone(numero: "789983889", modelo: "Iphone 14", imei: "365888472", memoria: 240);
+iphone.Ligar();
+iphone.InstalarAplicativo("Twitter");
